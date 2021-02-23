adccd4eee8f94a142585ff2e04aea0cb0e1a76b9
Vasyl Prystoiyni <prystoynyy@gmail.com>
2021-02-23 08:21:47 +0000

    Add contracts

---
+++ Rectangle.cs
@@ -0,0 +1,7 @@
+public class Rectangle
+{
+	public int X {get;set;}
+	public int Y {get;set;}
+	public int Width {get;set;}
+	public int Height {get;set;}
+}
---
+++ Tools.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+
+public static class Tools{
+    public static Rectangle FindRect(List<Point> points){
+        throw new NotImplementedException();
+    }
+
+}
